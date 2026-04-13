5007b665293e21cf66d426c255a2c49ae7c3245f
Seydina Wade <rassoulw01@gmail.com>
2026-04-13 11:54:34 +0000

    Ajout des Methodes De Base dans la classe GestionEtudiant

--- src/classes/gestionEtudiant.py
+++ src/classes/gestionEtudiant.py
@@ -3,9 +3,52 @@ import classes.etudiant.Etudiant as Etudiant
 class GestionEtudiant:
     
     def __init__(self):
-        self.listEtudiants=[]
+        self._listEtudiants=[]
         
-    def addEtudiant(self,currentEtudiant:Etudiant):
-        self.listEtudiants.append(currentEtudiant)
+    
+    def listEtudiants(self):
+        return self._listEtudiants  
     
     
+    def addEtudiant(self,currentEtudiant:Etudiant)->bool:
+        """_summary_
+
+        Args:
+            currentEtudiant (Etudiant): _description_
+
+        Returns:
+            bool: _description_
+        """
+        try:
+            self._listEtudiants.append(currentEtudiant)
+            return True
+        except :
+            return False
+        
+    def showEtudiant(self)->bool:
+        """_summary_
+
+        Returns:
+            bool: _description_
+        """
+        if not len(self._listEtudiants):
+            return False
+        for index,itemEtudiant in self._listEtudiants:
+            print(f"Etudiant : {index+1} {itemEtudiant}")
+            return True
+    
+    def classMoyenne(self)->float:
+        if not self.listEtudiants:
+            return None
+        
+        return sum([itemEtudiant._calculerMoyenne() for itemEtudiant in self._listEtudiants])/len(self._listEtudiants)
+
+    
+    def meilleurEtudiant(self) -> "Etudiant":
+        if not self._listEtudiants:
+            return None
+
+        return max(
+            self._listEtudiants,
+            key=lambda etu: etu.calculer_moyenne()
+        )
