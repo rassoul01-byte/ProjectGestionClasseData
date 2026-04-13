b7ec2c1395dc1bd2334a778767ef83bc6f0ca07d
Seydina Wade <rassoulw01@gmail.com>
2026-04-13 12:40:54 +0000

    Mise a jour de la classe MainApp

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ class MainApp:
     
     def run():
         MenuApp.showMenu()
-
+        while True:
+            try:
+                choice=int(input("Veuillez Selectionner votre Choix en valeur numerique : ".capitalize()))
+            except ValueError:
+                print("La Valeur entrer ne correspond pas a ce qui est attendu veuillez reessayer Merci")
+                continue
+            MenuApp.inputMenuChoice(choice)
+            
+            
 if __name__=="__main__":
     MainApp.run()
